e25dbbd1e7f69a7dcacbb1f5888694d005593998
KaWa <Karollo47@gmail.com>
2021-11-28 21:13:40 +0100

    added posibility to add users, need to create user friendly interface for it in html+css

--- src/main/java/pl/webser/users/UserService.java
+++ src/main/java/pl/webser/users/UserService.java
@@ -2,7 +2,13 @@ package pl.webser.users;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserService {
@@ -13,5 +19,13 @@ public class UserService {
     @Autowired
     ObjectMapper objectMapper;
 
-
+    @PostMapping(path = "/register")
+    public ResponseEntity addUser(@RequestBody User user){
+        Optional<User> userFromDb = userRepository.findByUserName(user.getUsername());
+        if (userFromDb.isPresent()){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
 }
