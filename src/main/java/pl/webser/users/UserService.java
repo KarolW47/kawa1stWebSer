35b64a2f5a6ce02fffcbde7b530b9751f70ec6be
KaWa <Karollo47@gmail.com>
2021-11-28 21:14:35 +0100

    minor changes to user service just for little test

--- src/main/java/pl/webser/users/UserService.java
+++ src/main/java/pl/webser/users/UserService.java
@@ -1,13 +1,16 @@
 package pl.webser.users;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,6 +22,14 @@ public class UserService {
     @Autowired
     ObjectMapper objectMapper;
 
+    //method for tests
+    @GetMapping(path = "/users")
+    public ResponseEntity getUsers () throws JsonProcessingException {
+        List<User> usersFromDb = userRepository.findAll();
+        return ResponseEntity.ok(objectMapper.writeValueAsString(usersFromDb));
+    }
+
+    //register new users method
     @PostMapping(path = "/register")
     public ResponseEntity addUser(@RequestBody User user){
         Optional<User> userFromDb = userRepository.findByUserName(user.getUsername());
