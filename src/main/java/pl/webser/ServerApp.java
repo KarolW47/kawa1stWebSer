5f6948e642f769d11ef0c6ba5e5e070874242cf0
KaWa <Karollo47@gmail.com>
2022-11-10 20:07:23 +0100

    updates before production ready

--- src/main/java/pl/webser/ServerApp.java
+++ src/main/java/pl/webser/ServerApp.java
@@ -26,36 +26,37 @@ public class ServerApp {
     CommandLineRunner run(UserService userService, RoleService roleService, PostService postService) {
         return args -> {
 
-            Role roleU = new Role();
-            Role roleM = new Role();
-            Role roleA = new Role();
-            roleU.setRoleName(ROLE_USER);
-            roleM.setRoleName(ROLE_MODERATOR);
-            roleA.setRoleName(ROLE_ADMIN);
-            roleService.addRole(roleU);
-            roleService.addRole(roleM);
-            roleService.addRole(roleA);
+            if(userService.getUserByEmailAddress("admin1@admin.ad") == null) {
+                Role roleU = new Role();
+                Role roleM = new Role();
+                Role roleA = new Role();
+                roleU.setRoleName(ROLE_USER);
+                roleM.setRoleName(ROLE_MODERATOR);
+                roleA.setRoleName(ROLE_ADMIN);
+                roleService.addRole(roleU);
+                roleService.addRole(roleM);
+                roleService.addRole(roleA);
 
-            User adminUser = new User();
-            adminUser.setUsername("admin1");
-            adminUser.setPassword("admin1");
-            adminUser.setEmailAddress("admin1@admin.ad");
-            userService.savePassedUser(adminUser);
+                User adminUser = new User();
+                adminUser.setUsername("admin1");
+                adminUser.setPassword("admin1");
+                adminUser.setEmailAddress("admin1@admin.ad");
+                userService.savePassedUser(adminUser);
 
-            userService.addRoleToRegisteredUser("admin1@admin.ad", ROLE_MODERATOR);
-            userService.addRoleToRegisteredUser("admin1@admin.ad", ROLE_ADMIN);
+                userService.addRoleToRegisteredUser("admin1@admin.ad", ROLE_MODERATOR);
+                userService.addRoleToRegisteredUser("admin1@admin.ad", ROLE_ADMIN);
 
-            postService.addPost("admin1@admin.ad", "Hello Everyone!");
+                postService.addPost("admin1@admin.ad", "Hello Everyone!");
 
 
-            User exampleUser = new User();
-            exampleUser.setUsername("example");
-            exampleUser.setPassword("example");
-            exampleUser.setEmailAddress("example@example.ex");
-            userService.savePassedUser(exampleUser);
+                User exampleUser = new User();
+                exampleUser.setUsername("example");
+                exampleUser.setPassword("example");
+                exampleUser.setEmailAddress("example@example.ex");
+                userService.savePassedUser(exampleUser);
 
-            postService.addPost("example@example.ex", "Example message.");
-
+                postService.addPost("example@example.ex", "Example message.");
+            }
         };
     }
 }
